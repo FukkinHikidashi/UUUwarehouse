7a70f74fa9c52edb2893273b0686afd3c767a0bc
FukkinHikidashi <s.t.fukkinghandson@gmail.com>
2021-06-19 14:46:27 +0900

    add mail.send()

--- test_sendmail.py
+++ test_sendmail.py
@@ -3,10 +3,13 @@ import win32com.client
 outlook = win32com.client.Dispatch("Outlook.Application")
 mail = outlook.CreateItem(0)
 
-mail.to = 'hazuki@mahodo.com; aiko@mahodo.com'
+mail.to = 'sho_takano@global.komatsu'
 mail.subject = 'UUUに入材の予定があります'
 mail.bodyFormat = 2
-mail.body = '''お疲れ様です。
-標記の件、UUU(K4U2S)倉庫への入材計画がありますので、
-UUUの状況をご連絡します。
-'''
+mail.body = "コンポG各位<br><br>\
+    お疲れ様です。<br>\
+    標記の件、UUU(K4U2S)倉庫への入材計画がありますので、\
+    UUUの状況をご連絡します。<br>\
+    <img src='photo/komatsu.jpg'>"
+
+mail.Send()
