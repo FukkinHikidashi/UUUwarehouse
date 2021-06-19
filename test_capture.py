b83a810f3dbfcd39820aaa3e4529f9a87491948b
FukkinHikidashi <s.t.fukkinghandson@gmail.com>
2021-06-19 16:10:42 +0900

    add photo sample

--- test_capture.py
+++ test_capture.py
@@ -5,10 +5,28 @@ import os
 # ネットワークカメラ接続方法を確認する
 # BoxDriveでいいよね？ Box保存にできるようにする？
 
-# fileName = os.getcwd() + datetime.datetime.today().strftime('%Y%m%d_%H%M%S') + ".png"
-fileName = "C:/Users/Are-y/OneDrive/Document/without/UUUscreenshot_" + datetime.datetime.today().strftime('%Y%m%d_%H%M%S') + ".png"
+
+def imwrite(filename, img, params=None):
+    try:
+        ext = os.path.splitext(filename)[1]
+        result, n = cv2.imencode(ext, img, params)
+
+        if result:
+            with open(filename, mode='w+b') as f:
+                n.tofile(f)
+            return True
+        else:
+            return False
+    except Exception as e:
+        print(e)
+        return False
+
+
+fileName = os.getcwd().replace(os.sep, '/') + "/UUUscreenshot_" + datetime.datetime.today().strftime('%Y%m%d_%H%M%S') + ".png"
+
 # 内蔵カメラのデバイスIDは0、USBで接続したカメラは1以降。
-capture = cv2.VideoCapture(0)
+capture = cv2.VideoCapture(0) # 内蔵カメラ用
+# capture = cv2.VideoCapture('protocol://username:password@IP:port/1') # portと/1ってなんだ 
 if not capture.isOpened():
     raise IOError("Cannot open webcam")
 # 取得した画像データは変数imageに格納。retは取得成功変数。
@@ -18,7 +36,7 @@ if not ret:
 if ret is True:
 
     # 取得した画像を出力。fileNameは出力する画像名。
-    ret = cv2.imwrite(fileName, image)
+    ret = imwrite(fileName, image)
     if not ret and not errmsg:
         errmsg = 'スクリーンショットの保存に失敗しました。'
 capture.release()
